8cd422d06975a146d64849ac227e0f5ebcda3940
Arga Tahta <bymac@Arga-Tahta.local>
2018-01-15 15:01:22 +0700

    update 1

--- index.js
+++ index.js
@@ -40,9 +40,13 @@ app.post("/api/validatetoken", passport.authenticate("auth", {session:false}), (
     res.send(req.user);
 });
 
+app.get("/",(req, res)=>{
+    res.send("Ready !");
+})
+
 app.use("/api/user", userRoutes);
 app.use("/api/event", eventRoutes(passport));
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log("connected");
 });
